43f5afd7597f4e78ff66734cc69661017e5c372c
KARAN-KADVANI <karankadvani2086@gmail.com>
2024-11-21 15:00:30 +0530

    Add files via upload

---
+++ assignment69.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int a[5]={1,2,3,4,5};
+    int x,flag=0;
+    int idx=0;
+
+    printf("enter any number which you want to search in the array : ");
+    scanf("%d",&x);
+    
+    
+    for(int i=0 ; i<5 ; i++)
+    {
+        if(a[i]==x)
+        {
+            flag=1;
+            idx=i;
+        }
+    }
+
+    if(flag==0)
+    {
+        printf("not present");
+    }
+
+    else{
+        printf("present and index is %d ", idx);
+    }
+}
---
+++ assignment70.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int a[n];
+    int x,flag=0;
+    int idx=0;
+
+    printf("enter any number which you want to search in the array : ");
+    scanf("%d",&x);
+    
+    
+    for(int i=0 ; i<5 ; i++)
+    {
+        if(a[i]==x)
+        {
+            flag=1;
+            idx=i;
+        }
+    }
+
+    if(flag==0)
+    {
+        printf("not present");
+    }
+
+    else{
+        printf("present and index is %d",idx);
+    }
+}
